f1e9f8b9ca2c0de3e8f7f99025ee1207127529ee
David Shin and Ilene Gorski <david@>
2017-04-06 16:01:07 -0700

    add cooler name

--- js/scripts.js
+++ js/scripts.js
@@ -28,27 +28,35 @@ $(function() {
       if(array[0]===array[1]&&array[1]===array[2]){
         alert(array[0] + " wins!");
         $("td").unbind("click");
+        $("#1, #2, #3").addClass("highlight");
       }if(array[3]===array[4]&&array[4]===array[5]){
         alert(array[3] + " wins!");
         $("td").unbind("click");
+        $("#4, #5, #6").addClass("highlight");
       }if(array[6]===array[7]&&array[7]===array[8]){
         alert(array[6] + " wins!");
         $("td").unbind("click");
+        $("#7, #8, #9").addClass("highlight");
       }if(array[0]===array[3]&&array[3]===array[6]){
         alert(array[0] + " wins!");
         $("td").unbind("click");
+        $("#1, #4, #7").addClass("highlight");
       }if(array[1]===array[4]&&array[4]===array[7]){
         alert(array[1] + " wins!");
         $("td").unbind("click");
+        $("#2, #5, #8").addClass("highlight");
       }if(array[2]===array[5]&&array[5]===array[8]){
           alert(array[1] + " wins!");
           $("td").unbind("click");
+          $("#3, #6, #9").addClass("highlight");
       }if(array[0]===array[4]&&array[4]===array[8]){
         alert(array[0] + " wins!");
         $("td").unbind("click");
+        $("#1, #5, #9").addClass("highlight");
       }if(array[2]===array[4]&&array[4]===array[6]){
           alert(array[2] + " wins!");
           $("td").unbind("click");
+          $("#3, #5, #7").addClass("highlight");
         }
   });
 });
